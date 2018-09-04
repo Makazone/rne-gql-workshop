fea70ce8b2f3bac3ad7fe86b7e28202f2cd81171
Makar Stetsenko <makazone@gmail.com>
2018-09-04 15:42:57 +0200

    Integrate Prisma

--- server/index.js
+++ server/index.js
@@ -1,47 +1,8 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { importSchema } = require("graphql-import");
+const { Prisma } = require("prisma-binding");
 
-const typeDefs = gql`
-  input UserInput {
-    id: ID!
-    name: String
-  }
-
-  type Mutation {
-    createUser(data: UserInput): User
-  }
-
-  type Query {
-    helloWorld: String!
-    getUser(id: ID!): User
-  }
-
-  type User {
-    id: ID!
-    name: String!
-  }
-
-  type Message {
-    id: ID!
-    body: String!
-    author: User!
-  }
-
-  type Chat {
-    id: ID!
-    messages: [Message!]!
-  }
-`;
-
-const users = {
-  id1: {
-    id: "id1",
-    name: "Josh"
-  },
-  id2: {
-    id: "id2",
-    name: "Fred"
-  }
-};
+const typeDefs = importSchema("schema.graphql");
 
 // Provide resolver functions for your schema fields
 const resolvers = {
@@ -58,7 +19,15 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async () => {
+    return {
+      prisma: new Prisma({
+        typeDefs: "generated/prisma.graphql",
+        endpoint: "https://eu1.prisma.sh/makar/prisma/dev"
+      })
+    };
+  }
 });
 
 server.listen({ port: 4000 }).then(({ url }) => {
